a4035488d9c2b28f9969a4fab144ecb679c349dc
tmartinez69009 <tmartinez69009@gmail.com>
2017-02-08 00:57:09 +0100

    First commit

---
+++ core/class/lametric.class.php
@@ -0,0 +1,99 @@
+<?php
+
+/* This file is part of Jeedom.
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+require_once dirname(__FILE__) . '/../../3rdparty/Lametric.php';
+require_once dirname(__FILE__) . '/../../3rdparty/LametricException.php';
+
+class lametric extends eqLogic {
+
+    /*     * ***********************Methode static*************************** */
+	
+	public function postSave() {	
+
+		$lametricCmd = $this->getCmd(null, 'message');
+		if (!is_object($lametricCmd)) {
+			$lametricCmd = new lametricCmd();
+			$lametricCmd->setName(__('Message', __FILE__));
+		}
+		$lametricCmd->setEqLogic_id($this->getId());
+		$lametricCmd->setLogicalId('message');
+		$lametricCmd->setType('action');
+		$lametricCmd->setSubType('message');
+		$lametricCmd->setDisplay('title_placeholder', __('ID de l\'icone', __FILE__));
+        $lametricCmd->setDisplay('message_placeholder', __('Texte', __FILE__));
+		$lametricCmd->setIsVisible(true);
+		$lametricCmd->save();
+		
+		$lametricCmd = $this->getCmd(null, 'clear');
+		if (!is_object($lametricCmd)) {
+			$lametricCmd = new lametricCmd();
+			$lametricCmd->setName(__('Vider', __FILE__));
+		}
+		$lametricCmd->setEqLogic_id($this->getId());
+		$lametricCmd->setLogicalId('clear');
+		$lametricCmd->setType('action');
+		$lametricCmd->setSubType('other');
+		$lametricCmd->setIsVisible(true);
+		$lametricCmd->save();
+
+	}
+	
+}
+
+class lametricCmd extends cmd {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    /*     * *********************Methode d'instance************************* */
+
+    
+
+    public function execute($_options = null) {
+    	log::add('lametric', 'info', 'Debut de l action');	
+		$lametricEq = $this->getEqLogic();
+    	$lametric2 = new Lametric2(array(
+		    'pushURL' => $lametricEq->getConfiguration('pushurl'),
+		    'token' => $lametricEq->getConfiguration('token'),
+		));
+    	if ($this->type == 'action' && isset($_options['message'])) {
+    		$messages = explode('|', $_options['message']);
+    		if(count($messages > 1)){
+    			$titles = explode('|', $_options['title']);
+    			$i = 0;
+    			foreach($messages as $message){
+					$lametric2->addFrame($message,$titles[$i]);
+					$i++;	
+				}
+    		}else{
+    			$lametric2->addFrame($_options['message'], $_options['title']);		
+    		}
+			$lametric2->push();
+			return true;
+		}else{
+			$lametric2->addFrame('JEEDOM', '4515');	
+			$lametric2->push();
+			return true;
+		}
+	}
+}
+?>
---
+++ desktop/php/lametric.php
@@ -0,0 +1,159 @@
+<?php
+if (!isConnect('admin')) {
+    throw new Exception('{{401 - Accès non autorisé}}');
+}
+
+$eqLogics=eqLogic::byType('lametric');
+sendVarToJS('eqType', 'lametric');
+
+?>
+
+<div class="row row-overflow">
+    <div class="col-lg-2">
+        <div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+   <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+   <div class="eqLogicThumbnailContainer">
+   		<div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+	       <i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
+	     <br>
+	     <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">Ajouter</span>
+	   </div>
+	   <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+		   <i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
+		   <br>
+		   <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+	   </div>
+  </div>
+        <legend><i class="techno-cable1"></i> {{Mes lametrics}}
+        </legend>
+        <?php
+        if (count($eqLogics) == 0) {
+            echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore de lametric, cliquez sur Ajouter pour commencer}}</span></center>";
+        } else {
+            ?>
+            <div class="eqLogicThumbnailContainer">
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+					$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+					echo '<img src="plugins/lametric/doc/images/lametric_icon.png" height="105" width="95" />';	
+                    echo "<br>";
+                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
+        <?php } ?>
+    </div>
+    <div class="col-lg-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+        <div class="row">
+        	<div class="col-sm-6">
+		        <form class="form-horizontal">
+		            <fieldset>
+		                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+		                <div class="form-group">
+		                    <label class="col-lg-2 control-label">{{Nom de l'équipement lametric}}</label>
+		                    <div class="col-lg-4">
+		                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+		                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement lametric}}"/>
+		                    </div>
+		                </div>
+		                <div class="form-group">
+		                    <label class="col-lg-2 control-label" >{{Objet parent}}</label>
+		                    <div class="col-lg-4">
+		                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+		                            <option value="">{{Aucun}}</option>
+		                            <?php
+		                            foreach (object::all() as $object) {
+		                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+		                            }
+		                            ?>
+		                        </select>
+		                    </div>
+		                </div>
+		                <div class="form-group">
+		                    <label class="col-lg-2 control-label">{{Catégorie}}</label>
+		                    <div class="col-lg-8">
+		                        <?php
+		                        foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+		                            echo '<label class="checkbox-inline">';
+		                            echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+		                            echo '</label>';
+		                        }
+		                        ?>
+		
+		                    </div>
+		                </div>
+		                <div class="form-group">
+					        <label class="col-sm-2 control-label"></label>
+					        <div class="col-sm-6">
+					          <input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer
+					          <input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible
+					        </div>
+				      </div>
+		              <div class="form-group">
+			            <label class="col-sm-3 control-label">{{Push URL}}</label>
+			            <div class="col-sm-5">
+			              <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="pushurl" placeholder="Push URL"/>
+			            </div>
+			          </div>
+			          <div class="form-group">
+			            <label class="col-sm-3 control-label">{{Token Access}}</label>
+			            <div class="col-sm-5">
+			              <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="token" placeholder="Token Access"/>
+			            </div>
+			          </div>
+		                
+		            </fieldset> 
+		        </form>
+			</div>
+			<div class="col-sm-6">
+				<legend><i class="fa fa-info"></i>  {{Informations}}</legend>
+                 <div class="form-group">	
+                    <div style="text-align: center">
+                     	<center><img src="plugins/lametric/doc/images/lametric_icon.png" id="img_Model"  onerror="this.src='plugins/lametric/doc/images/lametric_icon.png'" style="height : 280px;" /></center>
+                    </div>
+               	</div>
+			</div>
+		</div>		       
+
+        <legend>Commandes</legend>
+       <br/>
+        <table id="table_cmd" class="table table-bordered table-condensed">
+            <thead>
+                <tr>
+                    <th style="width: 200px;">{{Nom}}</th>
+                    <th style="width: 100px;">{{Type}}</th>
+                    <th style="width: 200px;">{{Options}}</th>
+                    <th style="width: 100px;"></th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="form-actions">
+                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+                </div>
+            </fieldset>
+        </form>
+
+    </div>
+</div>
+<?php include_file('desktop', 'lametric', 'js', 'lametric'); ?>
+<?php include_file('core', 'plugin.template', 'js'); ?>
